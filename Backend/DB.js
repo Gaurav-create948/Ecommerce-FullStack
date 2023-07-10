65d40667240e0b903ce82cce22eb3b6ed7db6a79
Gaurav Kumar <gauravrajpoot452@gmail.com>
2023-07-10 19:16:07 +0530

    solving database issue

--- Backend/DB.js
+++ Backend/DB.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host : '127.0.0.1',
+    host : 'https://ecommerce-app-jof9.onrender.com',
     user : 'root',
     password : 'Sonu@12345',
     database : 'user',
-    port : 3304     
+    port : 3306     
 })
 
 
